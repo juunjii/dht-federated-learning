f9bb353f065ced4b1c6a7e03209e0e46d0d837f8
Jun Hao Cheh <cheh0011@umn.edu>
2025-03-25 15:02:14 -0500

    Compute: Implemented skeleton code for put_data()

--- compute_node.py
+++ compute_node.py
@@ -2,6 +2,7 @@ import sys
 import time
 import random
 import glob
+from threading import Thread
 import os 
 
 sys.path.append('gen-py')
@@ -12,6 +13,7 @@ from thrift.transport import TSocket
 from thrift.transport import TTransport
 from thrift.protocol import TBinaryProtocol
 from thrift.server import TServer
+
 
 from compute import compute
 from compute.ttypes import WeightMatrices
@@ -39,9 +41,92 @@ class ComputeNodeHandler:
         self.finger_table = [None] * int(ceil(log2(MAX_NODES))) + 1
         self.finger_ids = [None] * int(ceil(log2(MAX_NODES))) + 1
 
-        # Training files
-        self.training_files = set()
+        # Tracks current files used for training
+        self.work= set()
 
         # Supernode information (for joining)
         self.supernode_host = supernode_host
         self.supernode_port = supernode_port
+
+
+    '''
+    Hash fname to numerical key value
+    '''
+    def hash_filename(self, fname):
+        pass
+    
+
+    '''
+    Reach the node responsible for the file
+    '''
+    def reach_destination(self, fname):
+        pass
+
+
+    '''
+    Forward the data to appropriate node 
+
+    Should return addrress of node to allow for recursive calls 
+    '''
+    def forward_data(self, key):
+        pass
+    
+    ''' 
+    Train model when data reaches node;
+    Stores trained weights, V and W for later retrieval
+    '''
+    def train(self, fname):
+        pass
+
+    '''
+    Connect to another node in the network
+    '''
+    def connect_to_node(self, host, port):
+        try:
+            transport = TSocket.TSocket(host, port)
+            transport = TTransport.TBufferedTransport(transport)
+            protocol = TBinaryProtocol.TBinaryProtocol(transport)
+            client = compute.Client(protocol)
+            transport.open()
+            return client, transport  # Returning both client and transport
+        except Exception as e:
+            print(f"Failed to connect to node {host}:{port} - {e}")
+            return None, None  # Handle failure
+    
+    def unpack_add(self, add):
+        host, port = add
+
+        return host, port
+
+    '''
+    Place input ML data (filename) into the network;
+    Recursively finds the destination node
+    '''
+    def put_data(self, fname):
+        # Hash fname to numerical key value
+        key = self.hash_filename(fname)
+        
+        # Reach destination node
+        if self.reach_destination(key):
+            print(f"Node {self.node_id} storing and training on file: {fname}")
+
+            # Add to work set
+            self.work.add(fname)
+            Thread(target=self.train, args=(fname)).start()
+        # Continue forwarding 
+        else:
+            # Return address of node (self.add = (host, port)
+            node = self.forward_data(key)
+            print(f"Node {self.node_id} forwarding file {fname} to node at {node}")
+
+            host, port = self.unpack_add(node)
+            client, transport = self.connect_to_node(host, port)
+
+            if client and transport:
+                try:
+                    client.put_data(fname)  
+                # Ensures that connection would be closed
+                finally: 
+                    transport.close() 
+            
+
