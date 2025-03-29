19708241882d6db5f67e9a6b27cc60c72351fec0
Braedon Nguyen <nguy4922@umn.edu>
2025-03-28 21:16:20 -0500

    improve node join

--- client.py
+++ client.py
@@ -5,6 +5,7 @@ import random
 import glob
 from collections import deque
 import numpy as np
+import time
 
 
 sys.path.append('gen-py')
@@ -15,58 +16,238 @@ from thrift.transport import TTransport
 from thrift.protocol import TBinaryProtocol
 from thrift.server import TServer
 
-from super import super
+from supernode import super
+from supernode.ttypes import Node
+from compute import compute
 
+from ML import *
 
-def main():
+class ClientHandler: 
+    def __init__(self, supernode_host='localhost', supernode_port=9091):
+        self.supernode_host = supernode_host
+        self.supernode_port = supernode_port
+        self.connection_point = None
+        
+    '''
+    Connect to another node in the network
+    '''
+    def connect_to_supernode(self):
+        try:
+            transport = TSocket.TSocket(self.supernode_host, self.supernode_port)
+            transport = TTransport.TBufferedTransport(transport)
+            protocol = TBinaryProtocol.TBinaryProtocol(transport)
+            client = super.Client(protocol)
+            transport.open()
+            return client, transport  
+        except Exception as e:
+            print(f"Failed to connect to node {self.supernode_host}:{self.supernode_port} - {e}")
+            return None, None  
     
-    # Make socket
-    transport = TSocket.TSocket('localhost', 9090)
-    transport = TTransport.TBufferedTransport(transport)
-    protocol = TBinaryProtocol.TBinaryProtocol(transport)
+    '''
+    Connect to another node in the network
+    '''
+    def connect_to_node(self, host, port):
+        try:
+            transport = TSocket.TSocket(host, port)
+            transport = TTransport.TBufferedTransport(transport)
+            protocol = TBinaryProtocol.TBinaryProtocol(transport)
+            client = compute.Client(protocol)
+            transport.open()
+            return client, transport  
+        except Exception as e:
+            print(f"Failed to connect to node {host}:{port} - {e}")
+            return None, None  
 
-    client = super.Client(protocol)
+    '''
+    Receive a connection point from supernode to join the network
+    Returns 0 on success; otherwise -1
+    '''
+    def join_network(self):
+        # connection_point = super.get_node() # Node(ip, port)
+        # conn_ip, conn_port = connection_point.ip, connection_point.port 
+        client, transport = self.connect_to_supernode()
 
-    try:
-        # Connect to super node 
-        transport.open()
+        if client and transport:
+            try:
+                # Get connection point
+                connection_point = client.get_node() # Node(ip, port)
+                conn_ip, conn_port = connection_point.ip, connection_point.port 
 
-        # Test the supernode functionality
-        print("Testing supernode functionality:\n")
+                if conn_ip is not None and conn_port is not None:
+                    self.connection_point = connection_point
+                    print(f"Connected to network via node at ip - {conn_ip} and port - {conn_port}")
+                    return 0
+                else:
+                    print(f"Fail to connect to network via node at ip - {conn_ip} and port - {conn_port}")
+                    return -1
+
+            # Ensures that connection would be closed
+            finally: 
+                transport.close() 
+        else:
+            print(f"Fail to connect to supernode")
+            return -1
+    
+    '''
+    Get files in a directory and store in list 
+    '''
+    def get_files(self, dir):
+        files = []
+        for f in os.listdir(dir):
+            if os.path.isfile(os.path.join(dir, f)):
+                fname = os.path.join(dir, f)
+                files.append(fname)
+
+        return files
+
+    '''
+    Distributes training files into the network of nodes 
+    Returns 0 on success; otherwise -1
+    '''
+    def distribute_data(self, dir):
+        # Sanitize
+        if not dir or not os.path.exists(dir) or not os.path.isdir(dir):
+            print(f"Error: Invalid directory '{dir}'")
+            return -1
         
-        # Print initial info
-        print("~~~~~~~~~~~~~~~~~~~~~~~~~")
-        print("Initial supernode info:")
-        print("~~~~~~~~~~~~~~~~~~~~~~~~~")
-
-        print(client.print_info())
-
-        node_id = client.request_join(8000)
-        print(f"Requested join with port 8000, got node ID: {node_id}")
+        if not self.connection_point:
+            print("Error: Not connected to network")
+            return -1
         
-        # Confirm join
-        client.confirm_join(node_id)
-        print(f"Confirmed join for node ID: {node_id}")
+        files = self.get_files()
+
+        print(f"Distributing {len(files)} files across the network...")
+
+        # Connect to connection point
+        conn_ip, conn_port = self.connection_point.ip, self.connection_point.port 
+        client, transport = self.connect_to_node(conn_ip, conn_port)
+
+        if client and transport:
+            try:
+                for f in files:
+                    print(f"Putting file {f} into network...")
+                    client.put_data(f)
+                print("Data distribution complete!")
+                return 0
+            # Ensures that connection would be closed
+            finally: 
+                transport.close() 
+    
+    '''
+    Get combined trained models from the network of compute nodes to be validated 
+    Returns 0 on success; otherwise -1
+    '''
+    def aggregrate_models(self, dir):
+        # Sanitize
+        if not dir or not os.path.exists(dir) or not os.path.isdir(dir):
+            print(f"Error: Invalid directory '{dir}'")
+            return -1
+
         
-        # Try to get a node
-        node_address = client.get_node()
-        print(f"Got node address: {node_address}\n")
+        if not self.connection_point:
+            print("Error: Not connected to network")
+            return -1
         
-        # Print updated info
-        print("~~~~~~~~~~~~~~~~~~~~~~~~~")
-        print("Updated supernode info:")
-        print("~~~~~~~~~~~~~~~~~~~~~~~~~")
-        print(client.print_info())
+        files = self.get_files()
+        
+        print(f"Collecting models for {len(files)} files from the network...")
 
-        # Close!
-        transport.close()
+        # Connect to connection point 
+        conn_ip, conn_port = self.connection_point.ip, self.connection_point.port 
+        client, transport = self.connect_to_node(conn_ip, conn_port)
 
-    except Thrift.TException as tx:
-        print(f"Thrift Exception: {tx.message}")
+        if client and transport:
+            try:
+                shared_gradient_V = None
+                shared_gradient_W = None
+                models_collected = 0
 
+                for f in files:
+                    # Tries to get model again if was not ready
+                    tries = 2
 
+                    for t in range(tries):
+                        print(f"Getting model for {f} (attempt {t+1})...")
+                        model = client.get_model(f)
+ 
+                    if model.status == "ready":
+                        print(f"Model for {f} has been aggregrated!")
+                        # Convert lists back to numpy arrays
+                        V = np.array(model.V)
+                        W = np.array(model.W)
+                        
+                        # Add to sums
+                        if shared_gradient_V is None:
+                            shared_gradient_V = V
+                            shared_gradient_W = W
+                        else:
+                            shared_gradient_V += V
+                            shared_gradient_W += W
+                        
+                        models_collected+= 1
+                        break
+                    elif model.status == "wait":
+                        print(f"Model for {f} is not ready, wait....")
+                        time.sleep(2)  # Wait for 2s before trying again
+                    else:  
+                        print(f"Model for {f} not found!")
+                        break
+
+                print(f"Aggregated {models_collected} models")
+
+                if models_collected == 0:
+                    print("No models collected")
+                    return -1
+                
+                # Average the models
+                shared_gradient_V = scale_matricies(shared_gradient_V, 1.0/models_collected)
+                shared_gradient_W = scale_matricies(shared_gradient_W, 1.0/models_collected)
+
+                # Validate aggregated model
+                model = mlp()
+                
+                validate_file = os.path.join(dir, "validate_letters.txt")
+                # Initialize model before training
+                initialized_model = model.init_training_model(validate_file, shared_gradient_V, shared_gradient_W)
+                if (initialized_model == False):
+                    raise Exception(f"Model initialization failed with file {f}")
+                validation_error = model.validate(validate_file)
+                print(f"Validation error: {validation_error}")
+
+                return 0
+        
+            # Ensures that connection would be closed
+            finally: 
+                transport.close() 
+    
+
+# Main client program
 if __name__ == '__main__':
-    try:
-        main()
-    except Thrift.TException as tx:
-        print('%s' % tx.message)
+    if len(sys.argv) != 3:
+        print("Usage: python client.py <training_dir> <supernode_port>")
+        sys.exit(1)
+    
+    training_dir = sys.argv[1]
+    supernode_port = sys.argv[2]
+
+    # Create client
+    client = ClientHandler(supernode_port=supernode_port)
+    
+    # Connect to network
+    if client.connect_to_network() == -1:
+        print("Failed to connect to network")
+        sys.exit(1)
+    
+    # Distribute data
+    if client.distribute_data(training_dir) == -1:
+        print("Failed to distribute data")
+        sys.exit(1)
+    
+    # Wait a bit for training to progress
+    print("Waiting for training to complete...")
+    time.sleep(10)
+    
+    # Aggregate models and validate
+    if client.aggregate_models(training_dir) == -1:
+        print("Failed to aggregate models")
+        sys.exit(1)
--- compute_node.py
+++ compute_node.py
@@ -207,7 +207,99 @@ class ComputeNodeHandler:
                 client.fix_fingers()
             finally:
                 transport.close()
+                
+    '''
+    Find corresponing predecessor for given node in network
+    '''
+    def find_predecessor(self, node_id):
+        # Sanitize
+        if not node_id:
+            return -1
+        
+        # One node in network
+        if self.succ_id == self.node_id:
+            return self.addr
+        
+        curr_id = self.node_id
+        curr_addr = self.addr
+
+        closest_entry = self.get_closest_finger_entry(node_id)
+        
+        # Continue searching until we find a node where input node is between it and its closest preceding node
+        while not self.is_between(node_id, curr_id, self.get_node_id(closest_entry)):
+            # If we're still at the current node
+            if curr_addr == self.addr:  
+                # Move to the closest preceding finger entry
+                curr_addr = self.get_closest_finger_entry(node_id)
+                curr_id = self.get_node_id(curr_addr)
+            # Connect to the remote node and ask for its closest preceding finger
+            else:
+                ip, port = self.unpack_add(curr_addr)
+                client, transport = self.connect_to_node(ip, port)
+                if client and transport:
+                    try:
+                        closest = client.get_closest_finger_entry(node_id)
+                        # Prevent infinite loop if cannot find suitable node
+                        if closest == curr_addr: 
+                            break
+                        # Move to the newly found closest node
+                        curr_addr = closest
+                        curr_id = self.get_node_id(curr_addr)
+                    # Ensures that connection would be closed
+                    finally: 
+                        transport.close() 
+        
+        return curr_addr
     
+    '''
+    Find the successor for specified node
+    '''
+    def find_successor(self, node_id):
+        if not node_id:
+            return -1
+
+        # If node id is between own node id and successor id 
+        if self.is_between(node_id, self.node_id, self.succ_id):
+            return self.succ_id
+        
+        # Find the closest preceding node
+        closest = self.get_closest_finger_entry(node_id)
+        
+        # No better routing option available 
+        if closest == self.addr:
+            return self.succ_id
+    
+    def get_node_id(self, address):
+        if address == self.addr:
+            return self.node_id
+            
+        if address == self.successor:
+            return self.successor_id
+            
+        if address == self.predecessor:
+            return self.predecessor_id
+            
+        # Check finger table
+        for i, addr in enumerate(self.finger_table):
+            if addr == address:
+                return self.finger_ids[i]
+        
+        # Address not in finger table 
+        # Query the network to get node id
+        try:
+            ip, port = self.unpack_add(address)
+            
+            client, transport = self.connect_to_node(ip, port)
+            if client and transport:
+                try:
+                    return client.get_id()
+                # Ensures that connection would be closed
+                finally: 
+                    transport.close() 
+        except:
+            print(f"Error: Failed to find node id for address - {address}")
+            return -1
+            
     def node_join(self):
         '''compute node joins the network'''
         # setup connection with supernode
@@ -246,14 +338,69 @@ class ComputeNodeHandler:
             try:
                 # set internal values to join
                 # TODO: Figure out what the error with getpred/succ is, add code to set pred_id and succ_id
+                # self.succ_id = client2.find_successor(self.node_id)
+                # self.succ = self.get_node_addreess(self.succ_id)
+                # self.pred_id = client2.find_predecessor()
+                # self.pred = self.get_node_addreess(self.pred)
                 self.succ = client2.get_successor()
                 self.pred = client2.get_predecessor()
+                self.pred_id = self.get_node_id(self.pred)
+                self.succ_id = self.get_node_id(self.succ)
                 self.fix_fingers()
                 client.confirm_join(self.node_id)
             finally:
                 transport2.close()
         transport.close()
+    
+    '''
+    Get corresponding address (host, port) of node by querying the network
+    '''
+    def get_node_addreess(self, node_id):
+        if not node_id:
+            return -1
 
+        if node_id == self.node_id:
+            return self.addr
+            
+        if node_id == self.succ_id:
+            return self.succ
+            
+        if node_id == self.pred_id:
+            return self.pred
+            
+        # Check finger table
+        for i, id in enumerate(self.finger_ids):
+            if id == node_id:
+                return self.finger_table[i]
+        
+        # Query the network to get successor info
+        succ_id = self.find_successor(node_id)
+
+        # Found node 
+        if succ_id == node_id:
+            succ_ip, succ_port = self.unpack_add(self.succ)
+            
+            client, transport = self.connect_to_node(succ_ip, succ_port)
+            if client and transport:
+                try:
+                    return client.get_predecessor()
+                # Ensures that connection would be closed
+                finally: 
+                    transport.close() 
+        
+        return self.successor  
+    
+    def get_responsible_key_range(self):
+        """Get the range of keys this node is responsible for"""
+        if self.predecessor_id is None:
+            return f"[0-{MAX_NODES-1}]"
+            
+        # Keys from (predecessor, self]
+        if self.predecessor_id < self.node_id:
+            return f"({self.pred_id}-{self.node_id}]"
+        else:  # Wrapping around
+            return f"({self.pred_id}-{MAX_NODES-1}] and [0-{self.node_id}]"
+        
     def print_info(self):
         '''prints node info'''
         
@@ -264,30 +411,32 @@ class ComputeNodeHandler:
         print("i", "Node", "ID")
         for i in range(len(self.finger_table)):
             print(i, self.finger_table[i], self.finger_ids[i])
-            
+        print(f"Responsible for keys: {self.get_responsible_key_range()}\n")
         print("Hashing info")
         print("Node ID: ", self.node_id)
         print("Files: ")
         for file in self.weights.keys():
             print(file, ", ")
     
-def get_successor(self):
-    """Returns the successor node"""
-    return self.succ
+    def get_successor(self):
+        """Returns the successor node"""
+        return self.succ
 
-def get_predecessor(self):
-    """Returns the predecessor node"""
-    return self.pred
+    def get_predecessor(self):
+        """Returns the predecessor node"""
+        return self.pred
 
 def main():
-    if len(sys.argv) != 3:
-        print("Usage: python3 compute_node.py <host> <port>")
+    if len(sys.argv) != 5:
+        print("Usage: python3 compute_node.py <host> <port> <supernode_host> <supernode_port>")
         sys.exit(1)
 
     host= sys.argv[1]
     port = int(sys.argv[2])
+    supernode_host = sys.argv[3]
+    supernode_port = int(sys.argv[4])
 
-    handler = ComputeNodeHandler(host, port)
+    handler = ComputeNodeHandler(host, port, supernode_host=supernode_host, supernode_port=supernode_port)
     processor = compute.Processor(handler)
     transport = TSocket.TServerSocket(host=host, port=port)
     tfactory = TTransport.TBufferedTransportFactory()
